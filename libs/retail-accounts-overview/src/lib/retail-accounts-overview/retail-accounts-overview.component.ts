f6ddd2775cb98e794edc89c8c70265f90a16c529
Akshay <akshay.mhatre8989@gmail.com>
2022-08-11 00:03:57 +0530

    feat: small template change

--- libs/retail-accounts-overview/src/lib/retail-accounts-overview/retail-accounts-overview.component.ts
+++ libs/retail-accounts-overview/src/lib/retail-accounts-overview/retail-accounts-overview.component.ts
@@ -2,8 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'banking-retail-accounts-overview',
-  template: ` <p>retail-accounts-overview works!</p> `,
+  template: ` <p>retail-accounts-overview works!!</p> `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RetailAccountsOverviewComponent{
-}
+export class RetailAccountsOverviewComponent {}
